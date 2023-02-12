abfddaf11ec81326d1e4c199a8ef9fa2d7eca5c4
Vladimir <mazeum1@gmail.com>
2023-02-13 04:37:41 +0700

    Program has finished

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void WordCutter(string[] array1, string[] array2)
+{
+    for (int i = 0, c = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length<=3)
+        {
+            array2[c] = array1[i];
+            c++;
+        }
+        
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите массив строк через пробел");
+string str = Console.ReadLine();
+string[] array1 = str.Split(' ');
+string[] array2 = new string[array1.Length];
+
+WordCutter(array1, array2);
+PrintArray(array2);
